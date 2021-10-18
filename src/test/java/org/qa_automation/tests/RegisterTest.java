8ee7c0b1ba4c5d7f919da9aa26bed90c7505033b
benyiannabella <benyiannabella@yahoo.com>
2021-10-18 19:29:59 +0300

    initial commit

---
+++ src/test/java/org/qa_automation/tests/RegisterTest.java
@@ -0,0 +1,8 @@
+package org.qa_automation.tests;
+
+public class RegisterTest {
+
+    
+
+
+}
---
+++ src/test/java/org/qa_automation/tests/WishListTest.java
@@ -0,0 +1,22 @@
+package org.qa_automation.tests;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class WishListTest {
+
+    @Test
+    public void addToWishlistTest() {
+        System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://testfasttrackit.info/selenium-test/");
+        driver.findElement(By.className("nav-5")).click();
+        driver.findElement(By.linkText("VIEW DETAILS")).click();
+        driver.findElement(By.className("link-wishlist")).click();
+
+    }
+}
