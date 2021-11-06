4a193fb8921743339de6a1db63bcbc9746ff55f2
benyiannabella@yahoo.com <benyiannabella@yahoo.com>
2021-11-06 20:56:24 +0200

    new page objects

---
+++ src/main/java/org/qa_automation/DriverFactory.java
@@ -0,0 +1,33 @@
+package org.qa_automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class DriverFactory {
+
+    public static WebDriver getWebDriver(String browser) {
+        WebDriver driver;
+
+        switch (browser) {
+            default:
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", AppConfig.getChromeDriverPath());
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", AppConfig.getGeckoDriverPath());
+                driver = new FirefoxDriver();
+                break;
+            case "ie":
+                System.setProperty("webdriver.ie.driver", AppConfig.getIEDriverPath());
+                driver = new ChromeDriver();
+                break;
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        return driver;
+    }
+}
